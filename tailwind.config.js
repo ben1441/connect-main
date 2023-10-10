c5e5574b702d628703e2012b5bb8623080a24645
ben1441 <manovah04@gmail.com>
2023-10-10 16:48:19 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sofia-sans-condensed': ['Sofia Sans Condensed', 'sans'],
+      }
+    },
+  },
+  plugins: [],
+}
+
